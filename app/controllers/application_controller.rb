b9f81d2f05e0bf4aa49f3b42a63c59a7bc765313
Romanch Sharma <romanchsharma@icloud.com>
2023-03-24 06:45:07 +0530

    add admin to users and Admin functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # redirect_to new_user_session_path unless current_user && current_user.admin?
 end
---
+++ app/models/admin.rb
@@ -0,0 +1,8 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  scope :all_admins, -> { where(admin: true).or(User.where(admin: true)) }
+end
