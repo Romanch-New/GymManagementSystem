3c4a3d5ad4c6b978179b16da1a2e8da94e0d6c42
Romanch Sharma <romanchsharma@icloud.com>
2023-03-24 15:34:51 +0530

    add scope for admin and users

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,4 @@
+# Admin model
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,4 +6,9 @@ class Admin < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :user, dependent: :destroy
+  # scope for users who have admin = true with all the admin from admin table
+  scope :admins, -> { joins(:user).where(users: { admin: true }) }
+
+  scope :users, -> { joins(:user).where(users: { admin: false }) }
+
 end
